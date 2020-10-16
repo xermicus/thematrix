8c2541abbad5991a86fdd716b393e42641821a22
xermicus <bigcyrill@hotmail.com>
2020-10-17 01:27:44 +0200

    make sled work

--- src/main.c
+++ src/main.c
@@ -11,18 +11,21 @@
 
 #define MATRIX_SIZE 256
 
-
 // needed for sled
-void sled_task(void* pvParameters, int core);
-int signal(int sign, void * hnd) {
-  return 0;
+//void sled_task(void *pvParameters, int core);
+//void sled_task(void *pvParameters);
+void user_main(void *pvParameters, int core);
+int signal(int sign, void *hnd)
+{
+    return 0;
 }
 
 void blink_task(void *pvParameter)
 {
     gpio_pad_select_gpio(BLINK_GPIO);
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-    while(1) {
+    while (1)
+    {
         gpio_set_level(BLINK_GPIO, 0);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
         gpio_set_level(BLINK_GPIO, 1);
@@ -30,29 +33,23 @@ void blink_task(void *pvParameter)
     }
 }
 
-void app_main()
+static void sled_task(void *pvParameter)
 {
-    xTaskCreate(&blink_task,        // task function
-        "blink_task",               // descriptive name of task
-        configMINIMAL_STACK_SIZE,   // stack depth
-        NULL,                       // arguments passed to task function's pvParameter argument
-        5,                          // priority of the task
-        NULL                        // created task handle
-    );
-
     gpio_pad_select_gpio(MATRIX_GPIO);
     gpio_set_direction(MATRIX_GPIO, GPIO_MODE_OUTPUT);
     gpio_set_level(MATRIX_GPIO, 0);
 
     strand_t pStrand;
-    pStrand.rmtChannel = 0;  
+    pStrand.rmtChannel = 0;
     pStrand.gpioNum = MATRIX_GPIO;
-    pStrand.ledType = LED_WS2812B_V2;
-    pStrand.brightLimit = 256;
+    pStrand.ledType = LED_WS2812B_V3;
+    pStrand.brightLimit = 200;
     pStrand.numPixels = MATRIX_SIZE;
     pStrand.pixels = NULL;
-    if (digitalLeds_initStrands(&pStrand, 1)) {
-        while (1) {
+    if (digitalLeds_initStrands(&pStrand, 1))
+    {
+        while (1)
+        {
             printf("init failure (digital led lib)\n");
             vTaskDelay(1000 / portTICK_PERIOD_MS);
         }
@@ -60,31 +57,24 @@ void app_main()
     digitalLeds_resetPixels(&pStrand);
     digitalLeds_updatePixels(&pStrand);
 
-    int i;
+    user_main(pvParameter, -1);
+}
 
-    while(1) {
-        for (i=0; i<MATRIX_SIZE; i++) {
-            pStrand.pixels[i].r = 255;
-            pStrand.pixels[i].g = 0;
-            pStrand.pixels[i].b = 0;
-        }
-        digitalLeds_updatePixels(&pStrand);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        for (i=0; i<MATRIX_SIZE; i++) {
-            pStrand.pixels[i].r = 0;
-            pStrand.pixels[i].g = 255;
-            pStrand.pixels[i].b = 0;
-        }
-        digitalLeds_updatePixels(&pStrand);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        for (i=0; i<MATRIX_SIZE; i++) {
-            pStrand.pixels[i].r = 0;
-            pStrand.pixels[i].g = 0;
-            pStrand.pixels[i].b = 255;
-        }
-        digitalLeds_updatePixels(&pStrand);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    
-    //sled_task(NULL, -1);
+void app_main()
+{
+    xTaskCreate(&blink_task,              // task function
+                "blink_task",             // descriptive name of task
+                configMINIMAL_STACK_SIZE, // stack depth
+                NULL,                     // arguments passed to task function's pvParameter argument
+                1,                        // priority of the task
+                NULL                      // created task handle
+    );
+
+    xTaskCreate(&sled_task,                    // task function
+                "sled_task",                   // descriptive name of task
+                configMINIMAL_STACK_SIZE * 12, // stack depth
+                NULL,                          // arguments passed to task function's pvParameter argument
+                10,                            // priority of the task
+                NULL                           // created task handle
+    );
 }
